6800838c53ee937040619d96a1218c79d95fbebb
udhav jindal <udhavjindal@udhavs-Laptop.local>
2025-06-20 15:20:27 +0530

    Initial commit: setup login backend with Express and MongoDB

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+
+exports.registerUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = new User({ email, password });
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    res.status(200).json({ message: 'Login successful' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
